98ebdc8c6f396fa0e803e0ee3b4b4554fbc292e8
lxexjx <eun.leee1377@gmail.com>
2022-04-18 15:50:01 +0900

    commit JDBC 이해

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+//상수니까 객체 생성안하도록
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
